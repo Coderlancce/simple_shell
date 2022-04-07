a5e4b286d8f0786f6af23d1185c6fb73f4085fb6
bcondict <jesus.junc10@gmail.com>
2022-04-07 12:27:42 -0500

    readme adds an change maxid Jesus

---
+++ excersice_before_proyect/README.md
@@ -0,0 +1,7 @@
+# some exersice before shell project
+
+| file | how excecute this file |
+| ------------- | ------------- |
+| getppid.c | gcc -Wall -Werror -pedantic getppid.c -o mypid && ./mypid |
+| maxid.c | gcc -Wall -Werror -pedantic maxid.c -o maxid && ./maxid pid_max |
+
--- excersice_before_proyect/maxid.c
+++ excersice_before_proyect/maxid.c
@@ -1,15 +1,19 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 /**
  *
  */
 
-int main()
+int main(int argc, char *argv[])
 {
 	int fd, n_read, n_dir;
 	char *buffer;
 
-	n_dir = chdir(/proc/sys/kernel/pid_max);
+	n_dir = chdir("/proc/sys/kernel");
 	if (n_dir == -1)
 		return (-1);
 
@@ -17,7 +21,7 @@ int main()
 	if (buffer == NULL)
 		return (-1);
 
-	fd = open(pid_max, RDONLY)
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		free (buffer);
@@ -30,7 +34,7 @@ int main()
 		close(fd);
 		return (-1);
 	}
-	printf("%u", buffer);
+	printf("%s", buffer);
 	close(fd);
 	return (0);
 }
