d31038a4a364d3f91bcdc2928db8994a8af0d47c
bcondict <jesus.junc10@gmail.com>
2022-04-07 13:10:37 -0500

    point 0 and readme changes

--- excersice_before_proyect/README.md
+++ excersice_before_proyect/README.md
@@ -6,4 +6,5 @@
 | ------------- | ------------- |
 | getppid.c | gcc -Wall -Werror -pedantic getppid.c -o mypid && ./mypid |
 | maxid.c | gcc -Wall -Werror -pedantic maxid.c -o maxid && ./maxid pid_max |
-
+| num_args.c | gcc -Wall -Werror -pedantic num_args.c -o num_args && ./ num_args "<any argument>" |
+ 
---
+++ excersice_before_proyect/num_args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints all arguments
+* @ac: count of given arguments
+* @av: arguments values
+* Return: Always 0 (success)
+*/
+int main (int ac, char *av[])
+{
+	int i = 0;
+
+	for (i = 0; av[i] != NULL; i++)
+		printf("%s\n", av[i]);
+	return (0);
+}
