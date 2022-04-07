663c2504eb4102bff2146b2b51da9d0cbe617cb3
bcondict <jesus.junc10@gmail.com>
2022-04-07 12:05:13 -0500

    changes Jesus Buffer and design

--- excersice_before_proyect/maxid.c
+++ excersice_before_proyect/maxid.c
@@ -7,28 +7,30 @@
 int main()
 {
 	int fd, n_read, n_dir;
-	char *buf;
+	char *buffer;
 
 	n_dir = chdir(/proc/sys/kernel/pid_max);
 	if (n_dir == -1)
 		return (-1);
 
-	buf = malloc(sizeof(char) * 10);
+	buffer = malloc(sizeof(char) * 10);
+	if (buffer == NULL)
+		return (-1);
 
 	fd = open(pid_max, RDONLY)
 	if (fd == -1)
 	{
-		free (buf);
+		free (buffer);
 		return (-1);
 	}
-	n_read = read(fd, buf, 10);
+	n_read = read(fd, buffer, 10);
 	if (n_read == -1)
 	{
-		free(buf);
+		free(buffer);
 		close(fd);
 		return (-1);
 	}
-	printf("%u", buf);
+	printf("%u", buffer);
 	close(fd);
 	return (0);
 }
