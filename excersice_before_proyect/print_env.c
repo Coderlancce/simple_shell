10eb68d5afe78c82bb44522ecda6e865586bd8ca
bcondict <jesus.junc10@gmail.com>
2022-04-10 19:42:17 -0500

    print path of executable version 2

--- excersice_before_proyect/print_env.c
+++ excersice_before_proyect/print_env.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
 *
@@ -6,12 +8,31 @@
 
 int main(int ac, char **av, char **env)
 {
-	unsigned int i = 0, j = 0;
-	char *buffer;
+	unsigned int i = 0, j = 0, current = 0, search = 0;
+	char *buffer = NULL, *buffer_get = NULL;
+	char *divide_str = NULL; 
+	int check = 0; 
+	size_t size = 32;
+
+	printf("$ ");
+	buffer_get = malloc(sizeof(char) * size);
+	if(buffer_get == NULL)
+		return (-1); 
+	check = getline(&buffer_get, &size, stdin);
+	if (check == -1)
+	{
+		free(buffer_get);
+		return (-1);
+	}
+	buffer_get[check - 1] = 0;
 
 	buffer = malloc(sizeof(char) * 1024);
 	if(buffer == NULL)
+	{
+		free(buffer_get);
 		return (-1);
+	}
+
 	for (i = 0; env[i] != NULL; i++)
 	{
 		if(env[i][0] == 'P' && env[i][1] == 'A')
@@ -21,15 +42,31 @@ int main(int ac, char **av, char **env)
 		}
 	}
 
-	divide_str = strtok(buffer, ':');
+	divide_str = strtok(buffer, ":");
 	if(divide_str == NULL)
 		return (-1);
 
 	while (divide_str != NULL)
+
 	{
-		printf("%s\n", divide_str);
-		divide_str = _strtok(NULL, ":");
+		
+		for (current = 0; divide_str[current]; current++) 
+		{
+			if (divide_str[current] == buffer_get[search])
+			{
+				for (search = 0; divide_str[current] == buffer_get[search]; current++, search++)
+				{
+					if (buffer_get[search] == 0)
+					{
+						printf("%s\n", divide_str);
+					}
+				}
+			} 
+		} 
+		divide_str = strtok(NULL, ":");
 	}
+	
+	free(buffer_get);
 	free(buffer);
 	return (0);
 }
