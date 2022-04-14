3b62e7e2724b18bca45b98696bf06c937bb05273
Coderlancce <coderlancce@gmail.com>
2022-04-14 15:56:36 -0500

    Welcome shell username print and ask

--- main.c
+++ main.c
@@ -7,15 +7,10 @@
 
 int main(void)
 {
-	int i = 0,
-	size_t size = 32;
-	char *username = NULL;
-	
-	create_buffer(username, size);
-	printf("Type your username");
-	generate_line(username, size);
-	while (i < 1)
-	{
-		fun_print = start_shell(username);
-	}
+	char *print_stat = NULL;
+
+	print_stat = fun_print();
+	printf("%s$", print_stat);
+
+	return (0);
 }
---
+++ print_stat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * fun_print - ask the username and send a buffer with that info
+ *
+ * Return: username, on frailure NULL
+ */
+
+char *fun_print()
+{
+	char *username = NULL;
+	int size = 10, check = 0;
+	size_t aux = 1;
+
+	username = malloc(sizeof(char) * size);
+	if (username == NULL)
+	{
+		printf("The buffer wasn't created correctly");
+		return (NULL);
+	}
+	printf("Type your username: ");
+	check = getline(&username, &aux, stdin);
+	if(check == -1)
+	{
+		printf("error");
+		return (NULL);
+	}
+	username[check - 1] = '\0';
+
+	return (username);
+}
